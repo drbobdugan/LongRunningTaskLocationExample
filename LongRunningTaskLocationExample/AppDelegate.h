a554276fed1d5e33c25de1ef4bc40133acb248a8
drbobdugan <bdugan@stonehill.edu>
2015-11-05 10:53:01 -0500

    Initial Commit

---
+++ LongRunningTaskLocationExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongRunningTaskLocationExample
+//
+//  Created by Bob Dugan on 11/5/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LongRunningTaskLocationExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongRunningTaskLocationExample
+//
+//  Created by Bob Dugan on 11/5/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
